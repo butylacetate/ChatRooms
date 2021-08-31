757e86f372d1225e3e9acbfe010ef135982065c9
butylacetate <butyl.acetate@gmail.com>
2021-08-31 20:44:20 +0400

    Initial commit

---
+++ src/main/java/com/example/chatrooms/ChatRoomsApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatrooms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatRoomsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatRoomsApplication.class, args);
+    }
+
+}
